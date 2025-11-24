c101477b6c6a166626758fa9595e80ca417447f9
edmar-zx <contato.edmarcozer756@gmail.com>
2025-11-24 01:56:01 -0400

    Atividade 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-// app/page.tsx (Server Component)
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // redireciona imediatamente para /products
   redirect("/products");
 }
